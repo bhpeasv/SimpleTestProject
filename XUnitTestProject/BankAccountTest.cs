1e76a17fdcae904ac620d37f78978fab379da82b
bhp <bhp@easv.dk>
2018-11-13 11:01:51 +0100

    Added XUnit tests

--- XUnitTestProject/BankAccountTest.cs
+++ XUnitTestProject/BankAccountTest.cs
@@ -1,26 +1,26 @@
-using NUnit.Framework;
+
 using SimpleTestProject;
 using System;
+using Xunit;
 
 namespace XUnitTestProject
-{
- 
+{   
     public class BankAccountTest
     {
-        [Test]
+        [Fact]
         public void CreateBankAccount_ValidAccNumber()
         {
             int accNumber = 1;
 
             BankAccount acc = new BankAccount(accNumber);
 
-            Assert.IsNotNull(acc);
-            Assert.AreEqual(accNumber, acc.AccNumber);
-            Assert.AreEqual(0.0, acc.Balance);
-            Assert.AreEqual(BankAccount.DEFAULT_INTEREST_RATE, acc.InterestRate);
+            Assert.NotNull(acc);
+            Assert.Equal(accNumber, acc.AccNumber);
+            Assert.Equal(0.0, acc.Balance);
+            Assert.Equal(BankAccount.DEFAULT_INTEREST_RATE, acc.InterestRate);
         }
 
-        [Test]
+        [Fact]
         public void CreateBankAccount_InvalidAccNumber_Expect_ArgumentException()
         {
             int accNumber = 0;
@@ -29,30 +29,30 @@ namespace XUnitTestProject
             {
 
                 acc = new BankAccount(accNumber);
-                Assert.Fail();
+                Assert.True(false);
             }
             catch (ArgumentException)
             {
-                Assert.IsNull(acc);
+                Assert.Null(acc);
             }
         }
 
-        [Test]
+        [Fact]
         public void CreateBankAccount_ValidInitialBalance()
         {
             double initialBalance = 1234.56;
             BankAccount acc = new BankAccount(1, initialBalance);
-            Assert.AreEqual(initialBalance, acc.Balance);
+            Assert.Equal(initialBalance, acc.Balance);
         }
 
-        [Test]
+        [Fact]
         public void CreateBankAccount_InvalidInitialBalance_ExceptArgumentException()
         {
             double initialBalance = -0.01;
             BankAccount acc = null;
             var ex = Assert.Throws<ArgumentException>(() =>  acc = new BankAccount(1, initialBalance));
-            Assert.That(ex.Message == "Initial balance must be above zero");
-            Assert.IsNull(acc);
+            Assert.True(ex.Message == "Initial balance must be above zero");
+            Assert.Null(acc);
         }
     }
 }
